43f82baf28c553ab8ab58f51749c9e0c3fb226b0
Roger Natan <152419074+Freakzn@users.noreply.github.com>
2025-05-27 18:05:39 -0300

    Add files via upload
    AV1 de FAC 1/2024 (material de estudo)

---
+++ Teste/av1_1FACN_2024.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+float calculaValor(int n_unidade, float valor_unidade){
+    return (n_unidade * valor_unidade);
+}
+int quantasPeloPrecoPromocional (int nt_unidade, int n_pagas_normal, int n_com_desconto){
+    grupos = n_pagas_normal + n_com_desconto;
+    grupos_completos = 
+    
+    
+    return;
+}
+float calculaExibeValorCompraTotal (int nt_unidade_comp, float valor_unidade, int n_pagas_normal, int n_com_deconto){
+    
+    printf("Unidades pagas noramlmente: %d\n", n_pagas_normal);
+    printf("Unidades pagas com promoção: %d\n", n_com_deconto);
+    printf("Valor total pago pelo preço normal: %d\n", valor_unidade);
+    printf("Valor pago por item promocional: %d\n")
+}
+
+
+
+int main(){
+    int n_unidade, nt_unidade, n_paga_normal, n_com_desconto, n_unidade_comp;
+    float valor_unidade;
+   
+    printf("Digite o valor total de unidades compradas: \n");
+    scanf("%d", &nt_unidade);
+    printf("Digite o preço por unidade: \n");
+    scanf("%f", &n_unidade);
+    printf("Digite o valor de unidades compradas pelo preço normal: \n");
+    scanf("%d", &n_paga_normal);
+    printf("Digite o valor de unidades compradas com desconto: \n");
+    scanf("%d", &n_com_desconto);
+
+
+    return 0;
+}
